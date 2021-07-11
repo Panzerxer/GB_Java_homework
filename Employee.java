7a051db0d658559f153d686683353305fac0ae14
Panzerxer <econ.evening2008@mail.ru>
2021-07-11 12:39:47 +0300

    wsw

---
+++ Employee.java
@@ -0,0 +1,34 @@
+package Lesson_5;
+
+public class Employee {
+    String fullname;
+    String position;
+    String email;
+    String phone;
+    int sallary;
+    int age;
+
+
+    Employee(String newfullname, String newposition, String newemail, String newphone, int newsallary, int newage){
+        fullname = newfullname;
+        position = newposition;
+        email = newemail;
+        phone = newphone;
+        sallary = newsallary;
+        age = newage;
+       // printer(); //Вывести всех
+        printerold(); //Вывести старшще 40 лет
+    }
+    void printer(){
+        System.out.println(fullname + " " + position + " " + email + " " + phone + " " + sallary + " " + age);
+        System.out.println();
+    }
+    void   printerold(){
+
+        if (age > 40){
+            System.out.println(fullname + " " + position + " " + email + " " + phone + " " + sallary + " " + age);
+        }
+
+    }
+
+}
